60390b18ad551568c495de562882ad6a9fcfb005
Nick Fernandez <nickfloresfernandez@gmail.com>
2018-01-05 17:23:29 -0500

    drawing single red spillover triangle, working on drawing several using several triangle positions

--- src2/draw_spillover_rects.js
+++ src2/draw_spillover_rects.js
@@ -1,43 +1,54 @@
-const vectorizeText = require('vectorize-text')
+// const vectorizeText = require('vectorize-text')
 
 module.exports = function draw_spillover_rects(regl, zoom_function){
   console.log('draw spillover_rects')
 
-  // max ~200 min ~20
-  var font_detail = 200;
-  var text_vect = vectorizeText('Spillover', {
-    textAlign: 'center',
-    textBaseline: 'middle',
-    triangles:true,
-    size:font_detail,
-    font:'"Open Sans", verdana, arial, sans-serif'
-  });
+  // // max ~200 min ~20
+  // var font_detail = 200;
+  // var text_vect = vectorizeText('Spillover', {
+  //   textAlign: 'center',
+  //   textBaseline: 'middle',
+  //   triangles:true,
+  //   size:font_detail,
+  //   font:'"Open Sans", verdana, arial, sans-serif'
+  // });
 
-  draw_function = regl({
-    vert: `
-      precision mediump float;
-      attribute vec2 position;
-      uniform mat4 zoom;
-
-      void main () {
-        // reverse y position to get words to be upright
-        gl_Position = zoom * vec4( 0.25*position.x, -0.25 * position.y + 1.2, 0.0, 2.0);
-      }`,
+  var args = {
+    // In a draw call, we can pass the shader source code to regl
     frag: `
-      precision mediump float;
-      void main () {
-        gl_FragColor = vec4(1, 0, 0, 1.0);
-      }`,
+    precision mediump float;
+    uniform vec4 color;
+    void main () {
+      gl_FragColor = color;
+    }`,
+
+    vert: `
+    precision mediump float;
+    attribute vec2 position;
+    void main () {
+      gl_Position = vec4(position, 0, 1);
+    }`,
+
     attributes: {
-      position: text_vect.positions
+
     },
-    elements: text_vect.cells,
+
     uniforms: {
-      zoom: zoom_function
-    }
-  })
+      color: [1, 0, 0, 1]
+    },
+
+    count: 3
+  }
+
+  args.attributes.position = [
+        [-1, 1],
+        [-0.5, -1],
+        [-1, -1]
+      ]
+
+  draw_function = regl(args)
+
 
   return draw_function;
-
 
 };
