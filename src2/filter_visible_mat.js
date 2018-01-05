e87a71f77b5b0985b699159e9e2a5f098f0f9e49
Nick Fernandez <nickfloresfernandez@gmail.com>
2018-01-05 16:28:04 -0500

    x and y filtering are working for matrix cells, working on row/col labels and dendros, as well as conservative implementation of filtering

--- src2/filter_visible_mat.js
+++ src2/filter_visible_mat.js
@@ -2,16 +2,29 @@ var $ = require('jquery');
 
 module.exports = function filter_visible_mat(arrs_orig, zoom_data){
 
+  /* Array re-calculation plan */
+  /*
+    Only re-calculate the array if a certain amount of zooming/panning has
+    occurred so that this will not slow things down too much
+  */
+
   var arrs = $.extend(true, {}, arrs_orig);
 
   // make a d3.scale to transition from 0px - 500px to -1, 1 space
-  var pix_to_webgl = d3.scale.linear();
-
   mat_width = 500;
 
-  pix_to_webgl
+  pix_to_webgl = {};
+
+  pix_to_webgl.x = d3.scale.linear();
+  pix_to_webgl.x
     .domain([0, mat_width])
     .range([-0.5, 0.5])
+    .clamp(true);
+
+  pix_to_webgl.y = d3.scale.linear();
+  pix_to_webgl.y
+    .domain([0, mat_width])
+    .range([0.5, -0.5])
     .clamp(true);
 
   // panning is defined as negative pixel values
@@ -19,39 +32,33 @@ module.exports = function filter_visible_mat(arrs_orig, zoom_data){
   total_pan.x_min = -zoom_data.x.total_pan_min;
   total_pan.x_max = mat_width + zoom_data.x.total_pan_max;
 
+  total_pan.y_min = -zoom_data.y.total_pan_min;
+  total_pan.y_max = mat_width + zoom_data.y.total_pan_max;
+
+  console.log(total_pan.y_min)
+
   buffer_width = 0.025;
 
   pan_webgl = {}
-  pan_webgl.x_min = pix_to_webgl(total_pan.x_min) - buffer_width;
-  pan_webgl.x_max = pix_to_webgl(total_pan.x_max) + buffer_width;
+  pan_webgl.x_min = pix_to_webgl.x(total_pan.x_min) - buffer_width;
+  pan_webgl.x_max = pix_to_webgl.x(total_pan.x_max) + buffer_width;
 
-  // console.log(pan_webgl)
+  pan_webgl.y_min = pix_to_webgl.y(total_pan.y_min) + buffer_width;
+  pan_webgl.y_max = pix_to_webgl.y(total_pan.y_max) - buffer_width;
 
-  /* Array re-calculation plan */
-  /*
-  Only re-calculate the array if a certain amount of zooming/panning has
-  occurred so that this will not slow things down too much
-  */
-
-  // // generating arrays from mat_data is very slow
-  // ///////////////////////////////////////////////
-  // var arrs = make_draw_cells_arr(regl, mat_data)
-
-  // // perform trivial slice of opacity array
-  // var num_keep = 100000;
-  // arrs.opacity_arr =   arrs.opacity_arr.slice(0, num_keep);
-  // arrs.position_arr = arrs.position_arr.slice(0, num_keep);
-
-
-  /*
-    visible area filtering is starting to work
-  */
 
   // filtering based on position
   keep_opacity = [];
   arrs.position_arr = _.filter(arrs.position_arr, function(d,i){
-    if (d[0] > pan_webgl.x_min && d[0] < pan_webgl.x_max){
-      console.log()
+
+    var check_x_min = d[0] > pan_webgl.x_min;
+    var check_x_max = d[0] < pan_webgl.x_max;
+
+    // y increases from top to bottom
+    var check_y_min = d[1] < pan_webgl.y_min;
+    var check_y_max = d[1] > pan_webgl.y_max;
+
+    if ( check_x_min && check_x_max && check_y_min && check_y_max){
       keep_opacity.push(arrs.opacity_arr[i])
       return d;
       }
