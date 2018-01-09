0d1b63bcbc6e72f799082bd7617ddfacd45f4d63
Nick Fernandez <nickfloresfernandez@gmail.com>
2018-01-09 16:26:42 -0500

    working on drawing several spillover triangles using instancing

--- src2/draw_spillover_rects.js
+++ src2/draw_spillover_rects.js
@@ -3,51 +3,99 @@
 module.exports = function draw_spillover_rects(regl, zoom_function){
   console.log('draw spillover_rects')
 
-  // // max ~200 min ~20
-  // var font_detail = 200;
-  // var text_vect = vectorizeText('Spillover', {
-  //   textAlign: 'center',
-  //   textBaseline: 'middle',
-  //   triangles:true,
-  //   size:font_detail,
-  //   font:'"Open Sans", verdana, arial, sans-serif'
-  // });
+  // // Spillover triangles
+  // ///////////////////////////////
 
-  var args = {
-    // In a draw call, we can pass the shader source code to regl
-    frag: `
-    precision mediump float;
-    uniform vec4 color;
-    void main () {
-      gl_FragColor = color;
-    }`,
+  // var args = {
+  //   // In a draw call, we can pass the shader source code to regl
+  //   frag: `
+  //   precision mediump float;
+  //   uniform vec4 color;
+  //   void main () {
+  //     gl_FragColor = color;
+  //   }`,
 
-    vert: `
-    precision mediump float;
-    attribute vec2 position;
-    void main () {
-      gl_Position = vec4(position, 0, 1);
-    }`,
+  //   vert: `
+  //   precision mediump float;
+  //   attribute vec2 position;
+  //   void main () {
+  //     gl_Position = vec4(position, 0, 1);
+  //   }`,
 
-    attributes: {
+  //   attributes: {
 
+  //   },
+
+  //   uniforms: {
+  //     color: [1, 0, 0, 1]
+  //   },
+
+  //   count: 3,
+  //   instances: 2
+  // }
+
+  // args.attributes.position = [
+  //       [-1, 1],
+  //       [-0.5, -1],
+  //       [-1, -1]
+  //     ];
+
+  // instancing example
+  //////////////////////////////
+  var N = 10 // N triangles on the width, N triangles on the height.
+
+  var offset_array = Array(N * N).fill().map((_, i) => {
+            var x = Math.floor(i / N) / N + 0.1 - 0.5
+            var y = 0
+            return [x, y]
+          })
+
+  args = {
+  vert: `
+  precision mediump float;
+  attribute vec2 position;
+
+  // These three are instanced attributes.
+  attribute vec3 color;
+  attribute vec2 offset;
+
+
+  void main() {
+    gl_Position = vec4(
+         position.x + position.y + offset.x,
+        -position.x + position.y + offset.y, 0, 1);
+  }`,
+
+  frag: `
+  precision mediump float;
+
+  void main() {
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  }`,
+
+  attributes: {
+    position: [[0.0, -0.05], [-0.05, 0.0], [0.05, 0.05]],
+
+    offset: {
+      buffer: regl.buffer(offset_array),
+      divisor: 1 // one separate offset for every triangle.
     },
 
-    uniforms: {
-      color: [1, 0, 0, 1]
-    },
+  },
 
-    count: 3
-  }
+  depth: {
+    enable: false
+  },
 
-  args.attributes.position = [
-        [-1, 1],
-        [-0.5, -1],
-        [-1, -1]
-      ]
+  // Every triangle is just three vertices.
+  // However, every such triangle are drawn N * N times,
+  // through instancing.
+  count: 3,
+  instances: N * N
+}
+
 
   draw_function = regl(args)
-
 
   return draw_function;
 
