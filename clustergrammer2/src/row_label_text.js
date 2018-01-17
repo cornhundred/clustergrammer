fec422fae974e9b088f43e0016019ba5b25fbd31
Nick Fernandez <nickfloresfernandez@gmail.com>
2018-01-17 13:11:10 -0500

    manually positioning labels, working on ordering, auto-positioning, zooming behavior

--- clustergrammer2/src/row_label_text.js
+++ clustergrammer2/src/row_label_text.js
@@ -4,7 +4,7 @@ module.exports = function row_label_text(params){
 
   var inst_nodes = params.network.row_nodes;
 
-  inst_nodes = inst_nodes.slice(0, 10);
+  // inst_nodes = inst_nodes.slice(0, 10);
 
   var num_rows = params.num_row;
 
@@ -43,7 +43,7 @@ module.exports = function row_label_text(params){
     var inst_name = inst_node.name.split(': ')[1];
 
     var tmp_text_vect = vectorizeText(inst_name, vect_text_attrs);
-    tmp_text_vect.offset = [-53.0, -y_offset_array[i]*1000/num_labels];
+    tmp_text_vect.offset = [-53.0, -y_offset_array[i]*3800/num_labels];
     outside_text_vect.push(tmp_text_vect);
 
   });
