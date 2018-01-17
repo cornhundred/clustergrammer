2649688a0e067e08e71b4161757328b12dd7073d
Nick Fernandez <nickfloresfernandez@gmail.com>
2018-01-17 11:52:25 -0500

    linted initialize_params

--- clustergrammer2/src/initialize_params.js
+++ clustergrammer2/src/initialize_params.js
@@ -8,11 +8,15 @@ var calc_spillover_positions = require('./calc_spillover_positions');
 
 var make_draw_cells_arr = require('./make_draw_cells_arr');
 var make_draw_cells_props = require('./make_draw_cells_props');
+var draw_mat_labels = require('./draw_mat_labels');
+var draw_dendro = require('./draw_dendro');
+var draw_text_triangles = require('./draw_text_triangles');
+var draw_spillover_rects = require('./draw_spillover_rects');
 
 module.exports = function initialize_params(regl, network){
 
-  console.log('** initialize_params')
-  var params = {}
+  // console.log('** initialize_params');
+  var params = {};
 
   // use data from network
   //////////////////////////
@@ -20,7 +24,7 @@ module.exports = function initialize_params(regl, network){
 
   var zoom_function = function(context){
     return context.view;
-  }
+  };
 
   params.still_interacting = false;
 
@@ -29,33 +33,28 @@ module.exports = function initialize_params(regl, network){
   params.num_row = params.mat_data.length;
   params.num_col = params.mat_data[0].length;
 
-  console.log('num_row: ' + String(params.num_row))
-  console.log('num_col: ' + String(params.num_col))
+  // console.log('num_row: ' + String(params.num_row));
+  // console.log('num_col: ' + String(params.num_col));
 
   // calculate the text_triangles for all rows
   params.text_triangles = row_label_text(network.row_nodes);
 
   params.draw_labels = {};
-  params.draw_labels['row'] = require('./draw_mat_labels')(regl, params.num_row, 'row');
-  params.draw_labels['col'] = require('./draw_mat_labels')(regl, params.num_col, 'col');
+  params.draw_labels.row = draw_mat_labels(regl, params.num_row, 'row');
+  params.draw_labels.col = draw_mat_labels(regl, params.num_col, 'col');
 
   params.draw_dendro = {};
-  params.draw_dendro['row'] = require('./draw_dendro')(regl, params.num_row, 'row');
-  params.draw_dendro['col'] = require('./draw_dendro')(regl, params.num_col, 'col');
+  params.draw_dendro.row = draw_dendro(regl, params.num_row, 'row');
+  params.draw_dendro.col = draw_dendro(regl, params.num_col, 'col');
 
-  params.draw_text_triangles = require('./draw_text_triangles')
-                                     (regl, zoom_function);
+  params.draw_text_triangles = draw_text_triangles(regl, zoom_function);
 
-  var draw_spillover_rects = {};
+  params.draw_spillover_rects = {};
 
   // inst_depth is passed to spillover rects
-  draw_spillover_rects.mat = require('./draw_spillover_rects')
-                                    (regl, zoom_function, 0.5);
+  params.draw_spillover_rects.mat = draw_spillover_rects(regl, zoom_function, 0.5);
 
-  draw_spillover_rects.corners = require('./draw_spillover_rects')
-                                        (regl, zoom_function, 0.4);
-
-  params.draw_spillover_rects = draw_spillover_rects;
+  params.draw_spillover_rects.corners = draw_spillover_rects(regl, zoom_function, 0.4);
 
   params.viz_dim = calc_viz_dim(regl);
 
@@ -70,11 +69,11 @@ module.exports = function initialize_params(regl, network){
 
   params.spillover_positions = calc_spillover_positions(params);
 
-  window.addEventListener('resize', params.cameras['mat'].resize);
+  window.addEventListener('resize', params.cameras.mat.resize);
   window.addEventListener('resize', params.cameras['row-labels'].resize);
 
   // generate position and opacity arrays from params.mat_data
-  var arrs = make_draw_cells_arr(regl, params)
+  var arrs = make_draw_cells_arr(regl, params);
 
   // generate draw_cells_props using buffers
   params.draw_cells_props = make_draw_cells_props(regl, params, arrs);
