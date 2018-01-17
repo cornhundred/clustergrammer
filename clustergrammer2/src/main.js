55467bbfc106cc0037c95dd080879b35a5868f7e
Nick Fernandez <nickfloresfernandez@gmail.com>
2018-01-17 13:13:17 -0500

    renamed matrix_example main

--- clustergrammer2/src/main.js
+++ clustergrammer2/src/main.js
@@ -2,15 +2,14 @@
   Making an interactive matrix using instancing.
 
   use the following to run using budo:
-  budo matrix_example.js --open --live -- -t es2020
+  budo main.js --open --live -- -t es2020
 
   use the following command to create a bundle:
-  browserify -r es2020 matrix_example.js > ../cytof_example_2/bundle.js
+  browserify -r es2020 main.js > ../cytof_example_2/bundle.js
 
   Bugs
   **************
-  1. resizing causes errors with tracking zooming/panning
-  2. resizing does not immediately redraw figure
+  1. resizing does not immediately redraw figure
 
  */
 
