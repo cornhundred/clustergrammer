4b85fdacd72d2b901ded7d6c700998732de71e7d
Nick Fernandez <nickfloresfernandez@gmail.com>
2018-01-18 09:53:49 -0500

    rotate working, but zooming stretching letters

--- clustergrammer2/src/make_col_text_triangle_args.js
+++ clustergrammer2/src/make_col_text_triangle_args.js
@@ -1,3 +1,5 @@
+var m3 = require('./mat3_transform');
+
 module.exports = function make_col_text_triangle_args(regl, params, zoom_function){
 
   var col_x_offset = d3.scale.linear()
@@ -5,6 +7,8 @@ module.exports = function make_col_text_triangle_args(regl, params, zoom_functio
     .range([-26.5, -53]);
 
   var x_offset = col_x_offset(params.text_zoom.col.inst_factor);
+
+  var mat_rotate = m3.rotation(Math.PI/4);
 
   var args = {
     vert: `
@@ -15,12 +19,15 @@ module.exports = function make_col_text_triangle_args(regl, params, zoom_functio
       uniform float x_offset;
       uniform float scale_y;
       uniform float width_scale;
+      uniform mat3 mat_rotate;
 
       // last value is a sort-of zoom
       void main () {
         // reverse y position to get words to be upright
         gl_Position = zoom *
                       vec4(
+
+                            mat_rotate *
 
                             vec3(
                                   (position.y) + offset[1] * scale_y,
@@ -44,7 +51,8 @@ module.exports = function make_col_text_triangle_args(regl, params, zoom_functio
       offset: regl.prop('offset'),
       x_offset: x_offset,
       scale_y: params.text_zoom.col.inst_factor,
-      width_scale: params.zoom_data.y.total_zoom
+      width_scale: params.zoom_data.y.total_zoom,
+      mat_rotate: mat_rotate
     },
     depth: {
       enable: true,
