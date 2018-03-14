c496770129df95044f63cd275bd98a827bf64639
Nick Fernandez <nickfloresfernandez@gmail.com>
2018-03-14 17:41:04 -0400

    reverted behavior, setting up selective rendering while interacting

--- clustergrammer2/src/make_draw_cells_arr.js
+++ clustergrammer2/src/make_draw_cells_arr.js
@@ -11,6 +11,22 @@ module.exports = function make_draw_cells_arr(regl, params){
   arrs.opacity_arr = opacity_arr;
   arrs.position_arr = position_arr;
 
+  // arrs.opacity_arr = arrs.opacity_arr.slice(10,20);
+  // arrs.position_arr = arrs.position_arr.slice(10,20);
+
+  console.log('hard filtering')
+  console.log(arrs.opacity_arr.length)
+
+  /*
+  Make initial array and then only keep elements that have opacity above some
+  value as a test. We might try to only render the top opacity elements while
+  zooming to speed up interaction.
+  */
+
+  _.each(arrs.opacity_arr, function(d){
+    console.log(d)
+  })
+
   return arrs;
 
 };
